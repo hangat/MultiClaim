96571364bb86633322897179ddc52a7f544db8b3
Hangat Perkasa <ihangat2000@gmail.com>
2021-12-04 21:55:10 +0700

    Add files via upload
    Source code MultiClaim
    Gak perlu password, ngak perlu cek ip

---
+++ inc.php
@@ -0,0 +1,24 @@
+<?php
+
+//===== Data yg dibutuhkan taruh disini saja =================
+$useragent = "Mozilla/5.0 (Linux; Android 7.1.2; G011A) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Mobile Safari/537.36";
+
+//===== Username ============
+$user="hangat";
+
+//===== ExpressCrypto ID =========
+$ecID="EC-UserId-587369";
+
+//===== Boost di: https://auto.multiclaim.net/autofaucet ============ 
+$bost1="10";
+
+//===== Boost di: https://bestautofaucet.com/session/autofaucet =====
+$bost2="5";
+
+//===== Boost di: https://proinfinity.fun/session/autofaucet ========
+$bost3="1";
+
+//===== Boost di: https://phoenixfaucets.xyz/session/autofaucet =====
+$bost4="5";
+
+?>
