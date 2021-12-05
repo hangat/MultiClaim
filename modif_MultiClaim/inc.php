2780034af57615966d1d46fb66036a01f7bf8cdb
Hangat Perkasa <ihangat2000@gmail.com>
2021-12-05 10:00:09 +0700

    Add files via upload

---
+++ modif_MultiClaim/inc.php
@@ -0,0 +1,25 @@
+<?php
+// xxxx : Disesuaikan 
+//===== Data yg dibutuhkan taruh disini saja =================
+$useragent = "xxxx";
+
+//===== Username ============
+$user="xxxx";
+
+//===== ExpressCrypto ID =========
+$ecID="xxxx";
+
+//===== Boost di: https://auto.multiclaim.net/autofaucet ============ 
+//sesuaikan saja bost nya spt boost yg di web
+$bost1="1";
+
+//===== Boost di: https://bestautofaucet.com/session/autofaucet =====
+$bost2="1";
+
+//===== Boost di: https://proinfinity.fun/session/autofaucet ========
+$bost3="1";
+
+//===== Boost di: https://phoenixfaucets.xyz/session/autofaucet =====
+$bost4="1";
+
+?>
