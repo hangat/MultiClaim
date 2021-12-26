2e4e28f32b8eaaf0f00115c0b7b8900096d38c9c
Hangat Perkasa <ihangat2000@gmail.com>
2021-12-26 08:01:27 +0700

    Add files via upload

--- modif/inc.php
+++ modif/inc.php
@@ -1,82 +1,193 @@
 <?php
 
 //===== Data yg dibutuhkan taruh disini saja =================
+//ini disesuaikan ya, kalo yg ini pakai pc, kalo pakai hp sptnya mesti disesuaikan
 $useragent = "Mozilla/5.0 (Linux; Android 7.1.2; G011A) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Mobile Safari/537.36";
 
 //===== Username ============
-$user="hangat";
+$user="xxxxxx";
 
 //===== ExpressCrypto ID =========
-$ecID="EC-UserId-587369";
+$ecID="EC-UserId-xxxxx";
 
+//==== utk ke ExpressCrypto.io : $payout_mode="ec" ======
+//$payout_mode="ec";
+
+//==== utk ke Faucetpay.io : $payout_mode="fp" ======
+$payout_mode="fp";
+
+//========================================================
 //===== Boost di: https://auto.multiclaim.net/autofaucet ============
 //$bost1="10";
-$bost1="5";
+$bost1="1";
+
+//$bost1="5";
+//$coins='"SHIB","BTT"';
+//===== Boost di: https://auto.multiclaim.net/autofaucet ============
+//$coins1='"SHIB","BTT"';
+//semua coins:
+$coins1='"BNB","BTT","USDT","RVN","BTC","BCH","BCN","ADA","DASH","DGB","DOGE","ETH","ETC","ZEN","KMD","LSK","LTC","XMR","NEO","PPC","PIVX","RDD","XRP","STRAX","XTZ","TRX","VTC","WAVES","ZEC","SHIB"';
+//$coins1='"BTT","USDT","DGB","DOGE","ETH","LTC","XRP","SHIB"';
+
+//"coins":["BNB","BTT","USDT","RVN","BTC","BCH","BCN","ADA","DASH","DGB","DOGE","ETH","ETC","ZEN","KMD","LSK","LTC","XMR","NEO","PPC","PIVX","RDD","XRP","STRAX","XTZ","TRX","VTC","WAVES","ZEC","SHIB"]
+//========================================================
+//"BNB","BTT","USDT","RVN","BTC","BCH","BCN","ADA","DASH","DGB","DOGE","ETH","ETC","ZEN","KMD","LSK","LTC","XMR","NEO","PPC","PIVX","RDD","XRP","STRAX","XTZ","TRX","VTC","WAVES","ZEC","SHIB"
+
+
+
+//========================================================
+//===== Boost di: https://bestautofaucet.com/session/autofaucet =====
+$bost2="1";
+//$bost2="5";
 
 //===== Boost di: https://bestautofaucet.com/session/autofaucet =====
-$bost2="3";
+// ditulis dengan huruf kecil utk bestautofaucet.com
+//$coins2='"dgb","btt"';
+//$coins2='"dgb","doge"';
+//$coins2='"dgb","doge"';
+//semua coin:
+//"coins":["doge","ltc","bch","dash","dgb","usdt","btt","xtz","waves","rvn","zec","xrp","ada","etc","bcn","xmr","bnb","pivx","Neo","Zen","Shib","VTC","PPC","KMD","STRAX","LSK"],
+//"doge","ltc","bch","dash","dgb","usdt","btt","xtz","waves","rvn","zec","xrp","ada","etc","bcn","xmr","bnb","pivx","Neo","Zen","Shib","VTC","PPC","KMD","STRAX","LSK"
+//coin tersedia saat ini:
+//$coins2='"usdt","etc","bcn","VTC","KMD","LSK"';
+$coins2='"doge","ltc","bch","dash","dgb","usdt","btt","xtz","waves","rvn","zec","xrp","ada","etc","bcn","xmr","bnb","pivx","Neo","Zen","Shib","VTC","PPC","KMD","STRAX","LSK"';
+//========================================================
 
+
+
+
+//========================================================
 //===== Boost di: https://proinfinity.fun/session/autofaucet ========
-$bost3="11";
+$bost3="1";
+//===== Boost di: https://proinfinity.fun/session/autofaucet ========
+//$coins3='"SHIB","BTT"';
+//$coins3='"dgb","btt"';
+//$coins3='"SHIB","TRX","DGB","BTT"';
+$coins3='"BTC","ETH","DOGE","TRX","DASH","DGB","BNB","ZEC","BCH","USDT","LTC","KMD","ADA","XRP","XMR","ZEN","RDD","STRAX","VTC","RVN","PIVX","BTT","LSK","XTZ","NEO","PPC","BCN","WAVES","ETC","FEY","EXS","SHIB","MATIC","SOL","XEC","EXG"';
+//"BTC","ETH","DOGE","TRX","DASH","DGB","BNB","ZEC","BCH","USDT","LTC","KMD","ADA","XRP","XMR","ZEN","RDD","STRAX","VTC","RVN","PIVX","BTT","LSK","XTZ","NEO","PPC","BCN","WAVES","ETC","FEY","EXS","SHIB","MATIC","SOL","XEC","EXG"
+//========================================================
 
+
+
+
+//========================================================
 //===== Boost di: https://phoenixfaucets.xyz/session/autofaucet =====
-$bost4="5";
+//gak bisa mode fp 
+$bost4="1";
+//$bost4="5";
 
 //====Coin yg diinginkan, Minimal 2 coin, Maximal 15, dipisah dengan Tanda koma,
 //contoh:  '"SHIB","BTT"'
 //bila ingin coin SHIB dan BTT
 //Ingat di apit oleh tanda petik satu ( ' );
-
-//$coins='"SHIB","BTT"';
-//===== Boost di: https://auto.multiclaim.net/autofaucet ============
-$coins1='"SHIB","BTT"';
-
-//===== Boost di: https://bestautofaucet.com/session/autofaucet =====
-// ditulis dengan huruf kecil utk bestautofaucet.com
-$coins2='"dgb","btt"';
-//$coins2='"dgb","doge"';
-//$coins2='"dgb","doge"';
-
-//===== Boost di: https://proinfinity.fun/session/autofaucet ========
-$coins3='"SHIB","BTT"';
-//$coins3='"dgb","btt"';
-//$coins3='"DGB","BTT"';
-
 //===== Boost di: https://phoenixfaucets.xyz/session/autofaucet =====
 //$coins4='"dgb","SHIB"';
-$coins4='"btt","SHIB"';
+//$coins4='"btt","SHIB"';
+
+$coins4='"SHIB","BCN","RDD","BTT","DGB","TRX","RVN","DOGE","VTC","PIVX","KMD","USDT","PPC","XRP","ADA"';
 
 //$coinAll ='"SHIB","BCN","RDD","BTT","DGB","TRX","RVN","DOGE","VTC","PIVX","KMD","USDT","PPC","XRP","ADA"';
 
 //Semua Coin:
 //"SHIB","BCN","RDD","BTT","DGB","TRX","RVN","DOGE","VTC","PIVX","KMD","USDT","PPC","XRP","ADA"
+//========================================================
 
+
+
+//========================================================
 //===== Boost di: https://dragonfaucets.xyz/ =====
-$bost5="5";
+$bost5="1";
+//$bost5="3";
 
 //==========================
 //$coins5='"ada","doge"';
 
 $coins5='"ada","doge","xmr","ltc","rvn","usdt","vtc","bch","pivx","lsk","dgb","zen","btt","kmd","xrp","neo","waves","etc","xtz","bnb","strax","zec","dash","ppc","trx"';
+//$coins5='"dgb","btt"';
+//========================================================
 
-//cookies: 2 coin
-/*
-_ga=GA1.2.991234225.1637897743; _pbjs_userid_consent_data=3524755945110770; cto_bundle=Kx_JkF9mUWVkOFNxJTJGY2k3Wmx0QVhFT0hNTHl2JTJGS0JXazdmODBRZ1U1cFRkc0hhZHpHQ05DT0EyJTJCY01aNCUyQlVVdnB6cmo4SWo4eUEyVjVWaEJ2ZXE2d1FwQ2JONHVjTmF2JTJCeCUyQkFmcWh6bkloZzBsbnptNHZSNUI3JTJCTmlqMXBrMTg3ZzM4UUpjQjMlMkZnSjBxUUd4UGM1QTlCeGp3JTNEJTNE; cto_bidid=k-beJF8xOG1iM1VHaTJWRUZQRU5ia2lsQlVvTUtjOWtiMEpCOUMwM3h0REt2b3F1OU1EZUJLciUyQktLbFVnZTJOQ2NNb3VDenZlWmc2OUozSVdwc2FoS0lCNFU1TWdvM3pGcGRnZUVBNzduNG5wVUwyUTNwRURyRUJZTHFqc1lIcGRDTGJt; _gid=GA1.2.209651153.1639592481; session_ok=true; cap=recaptcha; session=EC-UserId-587369; ab=0; __cf_bm=M6p9eeRxXEDUbWN_vsqU3zX61BA_RRjGEouWsRpLdO4-1639637357-0-AUhgtigNyBzQdg00X84aOdvtxP9t8CB8tsKTFJDKIiQqnYWBSW4Oo5hCR3QYXPv5P1jr7PApl+sS7AXgLr21OrJIdtRfwvm7PHQmRRz+QxsULVZwqyY5Ub/FJaRl6oVPJA==; auto={"ecid":"EC-UserId-587369","coins":["dgb","btt"],"mode":"multi","boost":"1","payout_mode":"ec"}; _gat_gtag_UA_179106564_3=1; _gat_gtag_UA_70132428_1=1; _data_cpc=505-1-1639641259; _data_html=26-1_93-1_648-1_655-1; __viCookieActive=true
-*/
 
-//cookies: multi coins
-/*
-_ga=GA1.2.991234225.1637897743; _pbjs_userid_consent_data=3524755945110770; cto_bundle=Kx_JkF9mUWVkOFNxJTJGY2k3Wmx0QVhFT0hNTHl2JTJGS0JXazdmODBRZ1U1cFRkc0hhZHpHQ05DT0EyJTJCY01aNCUyQlVVdnB6cmo4SWo4eUEyVjVWaEJ2ZXE2d1FwQ2JONHVjTmF2JTJCeCUyQkFmcWh6bkloZzBsbnptNHZSNUI3JTJCTmlqMXBrMTg3ZzM4UUpjQjMlMkZnSjBxUUd4UGM1QTlCeGp3JTNEJTNE; cto_bidid=k-beJF8xOG1iM1VHaTJWRUZQRU5ia2lsQlVvTUtjOWtiMEpCOUMwM3h0REt2b3F1OU1EZUJLciUyQktLbFVnZTJOQ2NNb3VDenZlWmc2OUozSVdwc2FoS0lCNFU1TWdvM3pGcGRnZUVBNzduNG5wVUwyUTNwRURyRUJZTHFqc1lIcGRDTGJt; _gid=GA1.2.209651153.1639592481; session_ok=true; cap=recaptcha; session=EC-UserId-587369; ab=0; __cf_bm=M6p9eeRxXEDUbWN_vsqU3zX61BA_RRjGEouWsRpLdO4-1639637357-0-AUhgtigNyBzQdg00X84aOdvtxP9t8CB8tsKTFJDKIiQqnYWBSW4Oo5hCR3QYXPv5P1jr7PApl+sS7AXgLr21OrJIdtRfwvm7PHQmRRz+QxsULVZwqyY5Ub/FJaRl6oVPJA==; __viCookieActive=true; _data_html=26-1_55-1_93-1_109-1_647-1_648-1_655-1; auto={"ecid":"EC-UserId-587369","coins":["ada","doge","xmr","ltc","rvn","usdt","vtc","bch","pivx","lsk","dgb","zen","btt","kmd","xrp","neo","waves","etc","xtz","bnb","strax","zec","dash","ppc","trx"],"mode":"multi","boost":"1","payout_mode":"ec"}; _gat_gtag_UA_179106564_3=1; _gat_gtag_UA_70132428_1=1; _data_cpc=69-1_77-1_505-1_506-1_551-1
-*/
-//request url:
-//https://dragonfaucets.xyz/session/autofaucet
 
-//referer:
-/*
-https://dragonfaucets.xyz/session/autofaucet
-*/
-//useragent:
-/*
-Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36
-*?
+//========================================================
+//===== Boost di: https://superautofaucet.com =====
+//gak bisa mode fp 
+$bost6="1";
+
+//==========================
+
+//$coins6='"BNB","Doge","USDT"';
+$coins6='"USDT"';
+//========================================================
+
+
+//========================================================
+//===== Boost di: https://mcmfaucets.xyz/ =====
+$bost7="1";
+//$bost7="30";
+
+//==========================
+//$coins7 = '"bnb","ada","usdt","xmr","xtz","lsk","trx","xrp","zec","bch","btt","etc","ltc","neo","strax","pivx","zen","dgb","kmd","ppc","waves","rvn","doge","bcn","rdd","vtc","dash","exg","exs","shib"';
+//saat ini yg tersedia
+//$coins7 = '"ada","xtz","lsk","trx","xrp","btt","etc","neo","strax","pivx","zen","kmd","ppc","waves","rvn","doge","bcn","rdd","vtc","dash","exg"';
+
+$coins7 = '"bnb","usdt","trx","zec","bch","ltc","dgb","doge","dash","fey","sol"';
+
+//dah 2 koin saja biar cepet bengkak...
+//$coins7 = '"trx","btt"';
+//$coins7 = '"btt","rvn","doge","shib"';
+
+//===== Boost di: https://auto.earnads.top/ =====
+//gak bisa mode fp 
+$bost8="1";
+$coins8 = '"BTT","DGB"';
+//Semua Coin:
+//"BNB","ADA","BCH","XMR","RVN","XTZ","XRP","ZEN","RDD","USDT","LTC","PIVX","KMD","BTT","DOGE","WAVES","ZEC","LSK","NEO","STRAX","PPC","TRX","VTC","ETC","DASH","DGB","SHIB"
+
+
+//===== Boost di: https://jupiterfaucet.net/ =====
+$bost9="1";
+$coins9 = '"dgb","btt"';
+//Semua Coin:
+//"shib","bch","ltc","trx","rvn","xmr","dgb","doge","btt","xrp","bnb","ada","zec","neo","exs","dash","rdd","kmd","waves","pivx","etc","lsk","ppc","zen"
+
+//===== Boost di: https://cryptolight.xyz/ =====
+$bost10="1";
+$coins10 = '"btt","dgb"';
+//Semua Coin:
+//"Btc","bnb","bch","btt","bcn","ada","dash","dgb","doge","Etc","Zen","Kmd","lsk","Ltc","Xmr","Neo","Ppc","Pivx","Rvn","Rdd","Xrp","Shib","Strax","Xtz","Trx","usdt","Vtc","Waves","Zec","Exs","Exg"
+
+//===== Boost di: https://alphafaucet.xyz/ =====
+$bost11="1";
+//$coins11 = '"BTT","DGB"';
+$coins11 = '"BNB","BTC","BCH","BTT","ADA","DASH","DGB","DOGE","ETH","ETC","EXG","EXS","ZEN","KMD","Lisk","LTC","XMR","NEO","PPC","PIVX","RVN","RDD","XRP","SHIB","STRAX","XTZ","TRX","USDT","VTC","WAVES","ZEC"';
+//Semua Coin:
+//"BNB","BTC","BCH","BTT","ADA","DASH","DGB","DOGE","ETH","ETC","EXG","EXS","ZEN","KMD","Lisk","LTC","XMR","NEO","PPC","PIVX","RVN","RDD","XRP","SHIB","STRAX","XTZ","TRX","USDT","VTC","WAVES","ZEC"
+
+
+//===== Boost di: https://claimcoins.club/ =====
+//gak bisa mode fp 
+$bost12="1";
+$coins12 = '"BTT","TRX"';
+//Semua Coin: 
+//"SHIB","BCN","EXS","BNB","BTT","RDD","TRX","DOGE","KMD","PIVX","LSK","RVN","xrp","PPC","XTZ","VTC","ADA","WAVES","DGB","USDT","ETC","NEO","STRAX","ZEN","LTC"
+
+//===== Boost di: https://cryptosway.xyz/ =====
+//gak bisa mode fp 
+$bost13="1";
+$coins13 = '"SHIB"';
+//semua Coin: "SHIB"
+
+//===== Boost di: https://auto.xtrabits.click/ =====
+$bost14="1";
+//$bost14="2";
+//$coins14 = '"ltc","shib","doge","bcn","btt"';
+//$coins14 = '"ltc","shib","doge","bcn","btt"';
+
+// utk mode fp, hanya tersedia BNB dan USDT
+$coins14 = '"bnb","usdt"';
+//$coins14 = '"ltc","shib","doge","bcn","btt"';
+//Semua Coin: 
+//"ltc","shib","doge","bcn","btt","rdd","trx","rvn","ada","dgb","xrp","LSK","ETC","ZEN","xtz","zec"
+
+
 ?>
